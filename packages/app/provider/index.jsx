425fd5d4c39dda6b32f3bd75bf149365c27775dc
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2022-04-27 16:02:28 +0200

    added color

--- packages/app/provider/index.jsx
+++ packages/app/provider/index.jsx
@@ -1,10 +1,13 @@
+import { Layout } from 'app/features/components/layout'
 import { Dripsy } from './dripsy'
 import { NavigationProvider } from './navigation'
 
 export function Provider({ children }) {
   return (
     <NavigationProvider>
-      <Dripsy>{children}</Dripsy>
+      <Dripsy>
+        <Layout>{children}</Layout>
+      </Dripsy>
     </NavigationProvider>
   )
 }

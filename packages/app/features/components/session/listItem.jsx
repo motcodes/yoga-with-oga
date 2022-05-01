2bf55daa3274a44762558c7e8ad190fb0e9a3a1d
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2022-05-01 23:01:39 +0200

    added list item

---
+++ packages/app/features/components/session/listItem.jsx
@@ -0,0 +1,36 @@
+import { Box, Flex, H3, Image, Text } from 'dripsy'
+import React from 'react'
+
+const DEFAULT_IMAGE =
+  'https://images.unsplash.com/photo-1567281150864-5296ada11f3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+
+export const ListItem = ({
+  imageUrl = DEFAULT_IMAGE,
+  title = 'Pose #1',
+  href = '/',
+}) => {
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: '2fr 3fr',
+        gap: 20,
+        alignItems: 'center',
+        height: 88,
+        width: '100%',
+      }}
+    >
+      <Image
+        source={imageUrl}
+        alt={title}
+        resizeMode="cover"
+        sx={{ borderRadius: 6, height: '100%' }}
+      />
+      <Flex sx={{ flexDirection: 'column', justifyContent: 'center' }}>
+        <Text as={H3} sx={{ color: '$green' }}>
+          {title}
+        </Text>
+      </Flex>
+    </Box>
+  )
+}

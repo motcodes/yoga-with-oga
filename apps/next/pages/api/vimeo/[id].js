0cf9ff5215a8bd17e5972c973dde7e0a416f6c65
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2022-05-03 16:46:37 +0200

    changed to vimeo config api

--- apps/next/pages/api/vimeo/[id].js
+++ apps/next/pages/api/vimeo/[id].js
@@ -1,25 +1,21 @@
-import { vimeoClient } from '../../../vimeo'
-
 export default async function vimeoHandler(req, res) {
   const {
+    method,
     query: { id },
   } = req
 
-  // vimeoClient.request(
-  //   {
-  //     path: `/video/${id}/config`,
-  //   },
-  //   (error, body) => {
-  //     console.log('body :', body)
-  //     if (!error) {
-  //       res.status(200).json(body)
-  //     }
-  //   }
-  const result = await fetch(`https://player.vimeo.com/video/${id}/config`)
-  const data = await result.json()
-  return res.status(200).json({
-    thumbnailUrl: data.video.thumbs['640'],
-    videoUrl: data.request.files.progressive[0].url,
-    video: data.video,
-  })
+  if (method === 'GET') {
+    const result = await fetch(`https://player.vimeo.com/video/${id}/config`)
+    const data = await result.json()
+
+    if (!data) {
+      return res.status(400).json('User not found')
+    }
+
+    return res.status(200).json({
+      thumbnailUrl: data.video.thumbs.base,
+      videoUrl: data.request.files.progressive[0].url,
+      duration: data.video.duration,
+    })
+  }
 }

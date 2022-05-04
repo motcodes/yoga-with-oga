415579568eb735b56fa760d274180aa5d7278620
Sebastian Hinterauer <fhs45884@fh-salzburg.ac.at>
2022-05-04 11:43:27 +0200

    Fixed SignIn bug

--- packages/app/features/auth/singIn.jsx
+++ packages/app/features/auth/singIn.jsx
@@ -52,14 +52,13 @@ export const SignIn = () => {
         }, 2000);
         }
     }
-  }
 
     const onSignUpSubmit = async (mail, password, { push }) => {
         try {
             const userCredentials = await signInWithEmailAndPassword(
-              auth,
-              mail,
-              password
+                auth,
+                mail,
+                password
             )
 
             const uId = userCredentials.user.uid
@@ -75,64 +74,63 @@ export const SignIn = () => {
             }, 2000);
         }
     }
-  }
 
-  return (
-    <SafeAreaView>
-      <ScrollView
-        sx={{
-          display: 'flex',
-          mx: 16,
-        }}
-      >
-        <InputErrorToast modalVisible={modalVisible} />
-
-        <View sx={{ height: 32 }} />
-
-        {/*<Logo />*/}
-
-        <View sx={{ height: 32 }} />
-
-        <Text variant={'base'} sx={{ color: '$green' }}>
-          New To Yoga with Oga?
-        </Text>
-        <Text variant={'small'} sx={{ color: '$yellow' }}>
-          Sign up for free
-        </Text>
-        <View sx={{ height: 16 }} />
-        <Input
-          value={state.mail.val}
-          onChange={(text) => dispatch({ type: 'mailChange', value: text })}
-          placeholder="Email address*"
-          style={{ borderColor: mailBC }}
-        />
-        <View sx={{ height: 12 }} />
-        <Input
-          value={state.password.val}
-          onChange={(text) => dispatch({ type: 'passwordChange', value: text })}
-          placeholder="Password*"
-          type="password"
-          style={{ borderColor: passwordBC }}
-          secureTextEntry={true}
-        />
-        <View sx={{ height: 24 }} />
-
-        <Button
-          onClick={() =>
-            onClickContinue(state.mail.val, state.password.val, {
-              push,
-              setModalVisible,
-              modalVisible,
-            })
-          }
+    return (
+        <SafeAreaView>
+        <ScrollView
+            sx={{
+            display: 'flex',
+            mx: 16,
+            }}
         >
-          Continue
-        </Button>
-        <View sx={{ height: 12 }} />
-        <Button variant={'text'}>
-          <TextLink href={'/auth/signUp'}>Sign Up</TextLink>
-        </Button>
-      </ScrollView>
-    </SafeAreaView>
-  )
+            <InputErrorToast modalVisible={modalVisible} />
+
+            <View sx={{ height: 32 }} />
+
+            {/*<Logo />*/}
+
+            <View sx={{ height: 32 }} />
+
+            <Text variant={'base'} sx={{ color: '$green' }}>
+            New To Yoga with Oga?
+            </Text>
+            <Text variant={'small'} sx={{ color: '$yellow' }}>
+            Sign up for free
+            </Text>
+            <View sx={{ height: 16 }} />
+            <Input
+            value={state.mail.val}
+            onChange={(text) => dispatch({ type: 'mailChange', value: text })}
+            placeholder="Email address*"
+            style={{ borderColor: mailBC }}
+            />
+            <View sx={{ height: 12 }} />
+            <Input
+            value={state.password.val}
+            onChange={(text) => dispatch({ type: 'passwordChange', value: text })}
+            placeholder="Password*"
+            type="password"
+            style={{ borderColor: passwordBC }}
+            secureTextEntry={true}
+            />
+            <View sx={{ height: 24 }} />
+
+            <Button
+            onClick={() =>
+                onClickContinue(state.mail.val, state.password.val, {
+                push,
+                setModalVisible,
+                modalVisible,
+                })
+            }
+            >
+            Continue
+            </Button>
+            <View sx={{ height: 12 }} />
+            <Button variant={'text'}>
+            <TextLink href={'/auth/signUp'}>Sign Up</TextLink>
+            </Button>
+        </ScrollView>
+        </SafeAreaView>
+    )
 }

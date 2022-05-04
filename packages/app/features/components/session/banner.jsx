4d7d9e71366b211a438da7b5e95f596a87583319
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2022-05-04 16:32:09 +0200

    updated homepage styling

--- packages/app/features/components/session/banner.jsx
+++ packages/app/features/components/session/banner.jsx
@@ -19,7 +19,7 @@ export function Banner({
     <Box
       sx={{
         width: '100%',
-        height: 320,
+        height: 408,
       }}
     >
       <ImageBackground
--- packages/app/features/home/screen.jsx
+++ packages/app/features/home/screen.jsx
@@ -1,4 +1,16 @@
-import { View, H1, ScrollView, SafeAreaView } from 'dripsy'
+import {
+  View,
+  H1,
+  ScrollView,
+  SafeAreaView,
+  Image,
+  Box,
+  H3,
+  H4,
+  Flex,
+  H5,
+  Text,
+} from 'dripsy'
 import React from 'react'
 import { Banner } from '../components/session'
 import { Link } from 'solito/link'
@@ -15,21 +27,52 @@ export function HomeScreen() {
           display: 'flex',
         }}
       >
-        <H1>
-          Welcome to Yoga with Oga{user && <span>, {user.firstName}</span>}
-        </H1>
-        <View sx={{ height: 32 }} />
+        <Flex
+          sx={{
+            pt: 24,
+            pb: 32,
+            px: 16,
+            flexDirection: 'column',
+          }}
+        >
+          <Image
+            source={{
+              uri: 'https://firebasestorage.googleapis.com/v0/b/yoga-with-oga.appspot.com/o/yoga-with-oga-logo-320.png?alt=media&token=edb09175-1e90-4633-adc1-5717bdf947fd',
+            }}
+            accessibilityLabel="Yoga with Oga Logo"
+            sx={{
+              width: 64,
+              height: 64,
+              alignSelf: 'center',
+              mb: '12px',
+            }}
+          />
+          <H4 as={H1} sx={{ color: '$green', mb: '8px' }}>
+            Get ready for some Yoga{user && <span>, {user.firstName}</span>}!
+          </H4>
+          <Text sx={{ color: '$greenLight' }}>
+            Only the best Sessions from Oga for you:
+          </Text>
+        </Flex>
         {sessions &&
-          sessions.map((item) => (
-            <Link href={`/session/${item.id}`} key={item.title}>
-              <Banner
-                title={item.title}
-                subtitle={item.subtitle}
-                imageUrl={item.imageUrl}
-                highlight="test"
-              />
-            </Link>
+          sessions.map((item, index) => (
+            <React.Fragment key={item.title}>
+              <Link href={`/session/${item.id}`}>
+                <Banner
+                  title={item.title}
+                  subtitle={item.subtitle}
+                  imageUrl={item.imageUrl}
+                  highlight={index === 0 ? 'Feature' : ' '}
+                />
+              </Link>
+              <Box sx={{ height: 48 }} />
+            </React.Fragment>
           ))}
+        <View sx={{ px: 16, pt: 24, pb: 96 }}>
+          <H5 sx={{ color: '$greenLight' }}>
+            more Sessions are comming soon...
+          </H5>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )

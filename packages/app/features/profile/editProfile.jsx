d6e7d3a6b8202695b07ddfcdc9c413203abc6ca0
Sebastian Hinterauer <fhs45884@fh-salzburg.ac.at>
2022-05-05 14:18:25 +0200

    Fixed merge bug

--- packages/app/features/profile/editProfile.jsx
+++ packages/app/features/profile/editProfile.jsx
@@ -1,15 +1,21 @@
-import { Text, View, ScrollView, SafeAreaView, Flex } from 'dripsy'
+import {
+    Text,
+    View,
+    ScrollView,
+    SafeAreaView,
+    Flex
+} from 'dripsy'
 import { useUser } from '../../provider/userContext'
 import { useRouter } from 'solito/router'
 import { Link } from 'solito/link'
 import { auth } from '../../firebase/client'
 import { SettingsListItem } from '../components/settingsListItem'
 import { useEffect, useState } from 'react'
-import { useEditProfile } from '../../helper'
+import { useEditProfile } from '../../helper/useEditProfile'
 import { Input } from '../components/input'
 import { Button } from '../components/button'
 import { InputErrorToast } from '../components/inputErrorToast'
-import { OnClickSave } from '../../helper/onClickSave'
+import { onClickSave } from '../../helper/onClickSave'
 
 import { db } from '../../firebase/client'
 import { doc, setDoc } from 'firebase/firestore'
@@ -26,103 +32,108 @@ export const EditProfile = () => {
             push('/')
         }
         else {
+            console.log(user)
             dispatch({ type: 'userNameChange', value: user.userName })
             dispatch({ type: 'firstNameChange', value: user.firstName })
             dispatch({ type: 'genderChange', value: user.gender === '' ? undefined : user.gender })
             dispatch({ type: 'heightChange', value: user.height === '' ? undefined : user.height })
             dispatch({ type: 'weightChange', value: user.weight === '' ? undefined : user.weight })
 
-      if (auth) setMail(auth.currentUser.email)
+            if (auth) setMail(auth.currentUser.email)
+        }
+    }, [user, auth])
+    
+    useEffect(() => {
+        if (state.userName.gotTargeted && state.userName.val === '') dispatch({ type: 'userNameBcChange' })
+        if (state.firstName.gotTargeted && state.firstName.val === '') dispatch({ type: 'firstNameBcChange' })
+    }, [state])
+
+
+    /*function onClickSave ({ state, push, user, setUser }) {
+        if (state.userName.val && state.firstName.val) {
+            onEditProfileSubmit({ state, push, user, setUser })
+        } else {
+        setModalVisible(true);
+        setTimeout(() => {
+            setModalVisible(false);
+        }, 2000);
+        }
     }
-  }, [user, auth])
+
+    async function onEditProfileSubmit ({ state, push, user, setUser }) {
+        try {
+            const { id, gender, height, weight, ...tmpUser } = user
+            let data = { ...tmpUser, userName: state.userName.val, firstName: state.firstName.val }
+            if (state.gender.val && state.gender.val !== '') data = { ...data, gender: state.gender.val}
+            if (state.height.val && state.height.val !== '') data = { ...data, height: state.height.val}
+            if (state.weight.val && state.weight.val !== '') data = { ...data, weight: state.weight.val}
+
+            await setDoc(doc(db, 'users', id), data)
+            setUser({ ...data, id: id })
+
+            push('/profile/settings')
+        } catch (error) {
+            console.log(error)
+
+            setModalVisible(true);
+            setTimeout(() => {
+                setModalVisible(false);
+            }, 2000);
+        }
+    }*/
 
     return (
         <SafeAreaView>
             <ScrollView>
                 <InputErrorToast modalVisible={modalVisible} />
-        <SettingsListItem title="Email" info={mail} />
-        <View sx={{ height: 16 }} />
 
-        <Flex
-          sx={{
-            flexDirection: 'column',
-            alignItems: 'center',
-            mx: 16,
-          }}
-        >
-          <Input
-            value={state.userName.value}
-            onChange={(text) =>
-              dispatch({ type: 'userNameChange', value: text })
-            }
-            placeholder="User Name"
-            style={{ borderColor: state.userNameBC }}
-          />
-          <View sx={{ height: 12 }} />
-          <Input
-            value={state.firstName.value}
-            onChange={(text) =>
-              dispatch({ type: 'firstNameChange', value: text })
-            }
-            placeholder="First Name"
-            style={{ borderColor: state.firstNameBC }}
-          />
-          <View sx={{ height: 12 }} />
-          <Input
-            value={state.gender.value}
-            onChange={(text) => dispatch({ type: 'genderChange', value: text })}
-            placeholder="Gender"
-            style={{ borderColor: '$grey80' }}
-          />
-          <View sx={{ height: 12 }} />
-          <Input
-            value={state.height.value}
-            onChange={(text) => dispatch({ type: 'heightChange', value: text })}
-            placeholder="Height"
-            style={{ borderColor: '$grey80' }}
-          />
-          <View sx={{ height: 12 }} />
-          <Input
-            value={state.weight.value}
-            onChange={(text) => dispatch({ type: 'weightChange', value: text })}
-            placeholder="Weight"
-            style={{ borderColor: '$grey80' }}
-          />
+                <View sx={{
+                    mx: 16,
+                    alignItems: 'center'
+                }}>
+                    <View sx={{ height: 58 }} />
+                    <Text variant={'h4'} sx={{ color: '$greenDark' }}>Edit Profile</Text>
+                    <View sx={{ height: 62 }}/>
+                </View>
+
+                <SettingsListItem title='Email' info={mail} />
+                <View sx={{ height: 16 }} />
+
                 <Flex sx={{
                     flexDirection: 'column',
                     alignItems: 'center',
                     mx: 16
                 }}>
                     <Input
-                    value={state.userName.val}
+                    value={state.userName.value}
                     onChange={text => dispatch({ type: 'userNameChange', value: text })}
                     placeholder="User Name"
                     style={{ borderColor: state.userNameBC }}
                     />
                     <View sx={{ height: 12 }} />
                     <Input
-                    value={state.firstName.val}
+                    value={state.firstName.value}
                     onChange={text => dispatch({ type: 'firstNameChange', value: text })}
                     placeholder="First Name"
                     style={{ borderColor: state.firstNameBC }}
                     />
                     <View sx={{ height: 12 }} />
                     <Input
-                    value={state.gender.val}
+                    value={state.gender.value}
                     onChange={text => dispatch({ type: 'genderChange', value: text })}
                     placeholder="Gender"
                     style={{ borderColor: '$grey80' }}
                     />
                     <View sx={{ height: 12 }} />
                     <Input
-                    value={state.height.val}
+                    value={state.height.value}
                     onChange={text => dispatch({ type: 'heightChange', value: text })}
                     placeholder="Height"
                     style={{ borderColor: '$grey80' }}
                     />
                     <View sx={{ height: 12 }} />
                     <Input
-                    value={state.weight.val}
+                    value={state.weight.value}
                     onChange={text => dispatch({ type: 'weightChange', value: text })}
                     placeholder="Weight"
                     style={{ borderColor: '$grey80' }}

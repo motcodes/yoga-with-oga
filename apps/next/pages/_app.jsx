1c0a8363ebeee8d8e7d245476b17d9bb55a75c87
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2022-05-20 12:01:08 +0200

    added seo

--- apps/next/pages/_app.jsx
+++ apps/next/pages/_app.jsx
@@ -11,9 +11,30 @@ function MyApp({ Component, pageProps }) {
         <title>Yoga with Oga</title>
         <meta
           name="description"
-          content="Yoga with Oga - by Matthias Oberholzer &amp; Sebastian Hinterauer"
+          content="A funny turn on a yoga app for beginners from beginners. You can choose from multiple sessions to fit your personal preference. Oga will perform in the yoga clips."
         />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00473e" />
+        <meta name="msapplication-TileColor" content="#f2f7f5" />
+        <meta name="theme-color" content="#f2f7f5"></meta>
       </Head>
       <Provider>
         <Component {...pageProps} />
